69d640eb53109edabd33d2bded74c456bfd837f5
CHRISSYBREEZY <112854576+CHRISSYBREEZY@users.noreply.github.com>
2023-05-03 02:16:07 +0200

    Uppdatering (0.0.2)
    - Fjernet boot.html
    - la til en knapp slik at du laster ned pdf

--- login.php
+++ login.php
@@ -8,8 +8,6 @@ $dbname = "users";
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
- 
-
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 }
@@ -26,7 +24,7 @@ $sql = "SELECT * FROM users WHERE email = '$email' AND password = '$password'";
  $result = $conn->query($sql);
 
     // Check if a user was found
-    if ($result->num_rows == 1) {
+if ($result->num_rows == 1) {
         // User was found, redirect to dashboard
 header("Location: dashboard.php");
 exit();
@@ -35,7 +33,5 @@ exit();
     // Bruker ikke funnet melding
     $error = "Ugyldig brukernavn eller passord";
 }}
-
-jallaballa
 
 
